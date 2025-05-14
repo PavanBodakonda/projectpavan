c32f5fbf0b27569dd2fccae05ed4a12b605c8646
Pavan <pavankumarbodakonda94@gmail.com>
2025-05-15 02:01:20 +0530

    Third COmmit

--- src/main/java/com/example/springbootawsdeploy/TestController.java
+++ src/main/java/com/example/springbootawsdeploy/TestController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/data")
     public String getData() {
-        return "First message from AWS Ecs";
+        return "This project is done by Pavan";
     }
 
     @GetMapping("/message")
     public String getMessage() {
-        return "Second message from AWS Ecs";
+        return "I'm happy with the learning";
     }
 }
